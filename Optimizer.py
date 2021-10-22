f5e930d667840dec78a45e4671bcb57e2a079b2c
Bas2904 <bastian.rasch@hotmail.com>
2021-10-22 13:46:37 +0200

    Hejhej

--- Optimizer.py
+++ Optimizer.py
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 import requests
 # getting HTML
 r = requests.get('/optimized-trips/v1/{profile}/{coordinates}')
 r.text; # HTML code
-=======
-print('Hello')
-print('Ok')
->>>>>>> 87f81192a92a27ffeeaf6ca420160c447b42b138
