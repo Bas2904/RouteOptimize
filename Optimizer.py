ccb2f34a30189f81cc9d3ea58533aba8b0a3a841
Bas2904 <bastian.rasch@hotmail.com>
2021-10-22 13:45:54 +0200

    API Initiliazier

--- Optimizer.py
+++ Optimizer.py
@@ -1,2 +1,9 @@
+<<<<<<< HEAD
+import requests
+# getting HTML
+r = requests.get('/optimized-trips/v1/{profile}/{coordinates}')
+r.text; # HTML code
+=======
 print('Hello')
-pritn('Ok')
+print('Ok')
+>>>>>>> 87f81192a92a27ffeeaf6ca420160c447b42b138
